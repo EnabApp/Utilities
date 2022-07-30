b4f2034ddd8ff83a74adc4d07780a8d5273cf20d
Hussein Najah <husseinnajah123@gmail.com>
2022-07-31 02:44:06 +0300

    Todo List
    I couldn't change the search bar height , still not used to use the new way for classes (new way for me) ... etc

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,24 +1,17 @@
 <template>
   <!-- Application -->
   <Transition>
-    <UiDesktopWindow  v-if="app.running" v-show="!app.minimized" :app="app">
-      <UiInput m="4" v-model="task"/>
-      <h1 text="w-100">
-        {{task}}
-      </h1>
-      <!-- <UiButton title="Hello" @click="getApp.toggleRunning()" /> -->
+    <UiDesktopWindow v-if="app.running" v-show="!app.minimized" :app="app">
+      <!-- I couldn't change the hight -->
+      <UiInput m="4" p="2" v-model="task" placeholder="ابحث هنا..." />
+      <!-- <TodoTodoList /> -->
     </UiDesktopWindow>
   </Transition>
 </template>
 
 <script setup>
-let task = ref(null)
-// const show = () =>{
-//   if(task.value != null){
-//     console.log(task.value)
-//     return true
-// }
-// }
+import { ref } from "vue";
+let task = ref(null);
 const props = defineProps({
   app: {
     type: Object,
@@ -26,10 +19,10 @@ const props = defineProps({
   },
 });
 watch(task, (val) => {
-  localStorage.setItem('task', val)
-})
+  localStorage.setItem("task", val);
+});
 onMounted(() => {
-  task.value = localStorage.getItem('task') || ''
-})
+  task.value = localStorage.getItem("task") || "";
+});
 const getApp = props.app.getSubApp(1);
 </script>
