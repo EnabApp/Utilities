68ca57c2275ae11c4337a828931c6d54cb53719f
hassan_000 <salem.cf11@gmail.com>
2022-07-31 11:39:08 +0300

    z

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,17 +1,24 @@
 <template>
   <!-- Application -->
   <Transition>
-    <UiDesktopWindow  v-if="app.running" v-show="!app.minimized" :app="app">
-      <UiInput m="4" v-model="task"/>
+    <UiDesktopWindow v-if="app.running" v-show="!app.minimized" :app="app">
+      <UiInput m="4" v-model="task" />
       <h1 text="w-100">
-        {{task}}
+        {{ task }}
       </h1>
-      <!-- <UiButton title="Hello" @click="getApp.toggleRunning()" /> -->
+      <Teleport to="body">
+        <UiModal v-model="stateModal" confirm="Save" @confirm="modalConfirmed" @cancel="modalCanceled">
+          <template v-slot:title>Modal Title</template>
+          Content
+        </UiModal>
+      </Teleport>
+       <UiButton title="showModel" @click="toggleModal()" /> -->
     </UiDesktopWindow>
   </Transition>
 </template>
 
 <script setup>
+
 let task = ref(null)
 // const show = () =>{
 //   if(task.value != null){
@@ -31,5 +38,17 @@ watch(task, (val) => {
 onMounted(() => {
   task.value = localStorage.getItem('task') || ''
 })
+
 const getApp = props.app.getSubApp(1);
+const [stateModal, toggleModal] = useToggle(false);
+
+const modalConfirmed = () => {
+  console.log("Confirmed");
+  stateModal.value = false;
+};
+
+const modalCanceled = () => {
+  console.log("Canceled");
+  stateModal.value = false;
+};
 </script>
