f239d3380cd48a28ada163a16a0524384ffdcc40
Hussein Najah <husseinnajah123@gmail.com>
2022-07-31 21:20:36 +0300

    Update Todo.vue

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -12,22 +12,9 @@
         placeholder="ابحث هنا..."
         icon="i-ant-design-search-outlined"
       />
-      <UiMenu 
-      w="52" 
-      bg="[#252c37]" 
-      mt="2" 
-      mr="6" 
-      title="الفلاتر">
-        <UiMenuItem 
-        bg="[#3c4651]" 
-        hover="bg-[#]" 
-        text="white" 
-        title="مهمة" />
-        <UiMenuItem 
-        bg="[#3c4651]" 
-        hover="bg-[#]" 
-        text="white" 
-        title="مكتمله" />
+      <UiMenu w="52" bg="[#252c37]" mt="2" mr="6" title="الفلاتر">
+        <UiMenuItem bg="[#3c4651]" hover="bg-[#]" text="white" title="مهمة" />
+        <UiMenuItem bg="[#3c4651]" hover="bg-[#]" text="white" title="مكتمله" />
         <UiMenuItem
           bg="[#3c4651]"
           hover="bg-[#]"
@@ -41,7 +28,7 @@
           title="قيد التنفيذ"
         />
       </UiMenu>
-      <TodoList bg="black" />
+      <TodoList />
       <Teleport to="body">
         <UiModal
           v-model="stateModal"
@@ -49,8 +36,60 @@
           @confirm="modalConfirmed"
           @cancel="modalCanceled"
         >
-          <template v-slot:title>Modal Title</template>
-          Content
+          <template v-slot:title>
+            <div ref="titleEl">
+              <h4 v-if="stateTitle" text="w-100">
+                <UiInput h="20" v-model="task" />
+              </h4>
+              <div v-if="!stateTitle" @click="toggleTitle()">
+                <div flex="~" items="center">
+                  <h4 text="w-100">
+                    {{ task }}
+                  </h4>
+                  <div class="i-fa6-solid:pen text-xl mr-3"></div>
+                </div>
+              </div>
+            </div>
+          </template>
+          <div flex="~ col">
+            <div class="overflow-y-auto h-90 w-full">
+              <textarea
+                ref="textarea"
+                bg="transparent"
+                text="w-100"
+                border="none"
+                v-model="input"
+                class="resize-none outline-none placeholder:text-w-100"
+                cols="100"
+                rows="10"
+                placeholder="التفاصيل"
+              />
+            </div>
+          </div>
+          <div v-if="!stateSidebar" class="flex justify-center relative">
+            <div
+              @click="toggleSidebar()"
+              class="i-bi:arrow-up-square-fill absolute top-15"
+              text="xl w-100"
+            ></div>
+          </div>
+          <Transition>
+            <div
+              v-if="stateSidebar"
+              class="border rounded-lg flex flex-col mt-3 items-center h-20"
+            >
+              <div
+                @click="toggleSidebar()"
+                class="i-bi:arrow-down-square-fill"
+                text="xl w-100"
+              ></div>
+              <div class="flex">
+                <div></div>
+                <div>df</div>
+                <div>df</div>
+              </div>
+            </div>
+          </Transition>
         </UiModal>
       </Teleport>
       <div
@@ -78,22 +117,36 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 let search = ref(null);
+
+const titleEl = ref(null);
+
+const { textarea, input } = useTextareaAutosize();
+
+const getApp = props.app.getSubApp(1);
+
+const [stateModal, toggleModal] = useToggle(false);
+
+const [stateSidebar, toggleSidebar] = useToggle(false);
+
+const [stateTitle, toggleTitle] = useToggle(false);
+
 const props = defineProps({
   app: {
     type: Object,
     required: true,
   },
 });
+
 watch(search, (val) => {
   localStorage.setItem("search", val);
 });
+
 onMounted(() => {
   search.value = localStorage.getItem("search") || "";
 });
-
-const getApp = props.app.getSubApp(1);
-const [stateModal, toggleModal] = useToggle(false);
 
 const modalConfirmed = () => {
   console.log("Confirmed");
@@ -104,4 +157,20 @@ const modalCanceled = () => {
   console.log("Canceled");
   stateModal.value = false;
 };
+
+onClickOutside(titleEl, () => {
+  stateTitle.value = false;
+});
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
