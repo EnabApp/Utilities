2f0a57d29604f0348bc1564b590bfff356cd5e81
Hussein Najah <husseinnajah123@gmail.com>
2022-07-31 13:13:49 +0300

    card/list + filter

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,8 +3,23 @@
   <Transition>
     <UiDesktopWindow v-if="app.running" v-show="!app.minimized" :app="app">
       <!-- I couldn't change the hight -->
-      <UiInput m="4" p="2" v-model="task" placeholder="ابحث هنا..." />
-      <!-- <TodoTodoList /> -->
+      <UiInput
+        m="4"
+        p="2"
+        pb="12"
+        h="4"
+        v-model="task"
+        placeholder="ابحث هنا..."
+        icon="i-ant-design-search-outlined"
+      />
+      <UiMenu class="bg-[#252C37] w-52" mt="2" mr="6" title="الفلاتر" 
+      >
+        <UiMenuItem  title="مهمة" />
+        <UiMenuItem  title="مكتمله" />
+        <UiMenuItem  title=" غير مكتمله" />
+        <UiMenuItem  title="قيد التنفيذ" />
+      </UiMenu>
+      <TodoList />
     </UiDesktopWindow>
   </Transition>
 </template>
---
+++ src/components/Todo/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="w-52 h-56 bg-[#252C37] rounded-lg shadow-sm border-t-7 border-green-400"
+    m="6"
+    mt="10"
+  >
+    <div class="p-2 text-center flex flex-col gap-5">
+      <!-- Title -->
+      <h3 class="font-semibold" text="white">عرس حسن</h3>
+      <div>
+        <!-- Description -->
+        <p text="gray-300" class="text-sm">
+          `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          euismod, nisi eu consectetur consectetur, nisl nisi consectetur nisl,
+          eu consectetur nisl nisi...
+        </p>
+      </div>
+      <div class="flex justify-end mt-3">
+        <!-- Date -->
+        <p text="gray-400" class="text-xs">من 2022/23/2 الى 2023/24/8</p>
+      </div>
+    </div>
+  </div>
+</template>
