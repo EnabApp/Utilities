4ab822d6f73c6f6a4e1e5761553bbadc9682bde1
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-12 21:43:00 +0300

    asd

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
         MyModule,
         '@enab/ui',
         '@enab/core',
+        '@nuxtjs/supabase',
         '@pinia/nuxt',
         '@vueuse/nuxt',
         '@unocss/nuxt',
--- src/runtime/components/Todo.vue
+++ src/runtime/components/Todo.vue
@@ -8,39 +8,15 @@
             <div flex="~ col">
               <!-- ===========>> Add InPut Content <<=========== -->
               <div>
-                <UiInput
-                  m="y-20px"
-                  placeholder="اضافه مهمة ..."
-                  v-model="title"
-                  @keydown.enter="addTodo()"
-                />
+                <UiInput m="y-20px" placeholder="اضافه مهمة ..." v-model="title" @keydown.enter="addTodo()" />
               </div>
-              <div
-                m="t-5px"
-                flex="~ col gap-30px"
-                w="full"
-                overflow="y-scroll"
-                h="315px"
-              >
+              <div m="t-5px" flex="~ col gap-30px" w="full" overflow="y-scroll" h="315px">
                 <!-- ===========>> Todo Content <<=========== -->
                 <div v-for="todo in todos_a" :key="todo">
-                  <div
-                    id="todoDiv"
-                    flex="~"
-                    class=""
-                    justify="between"
-                    items="center"
-                  >
+                  <div id="todoDiv" flex="~" class="" justify="between" items="center">
                     <TodoTask :todo="todo" />
                     <!-- ===========>> Remove Button Content <<=========== -->
-                    <UiButton
-                      id="markHover"
-                      duration="250"
-                      w="70px"
-                      title="حذف"
-                      color="error"
-                      @click="removeTodo(todo)"
-                    />
+                    <UiButton id="markHover" duration="250" w="70px" title="حذف" color="error" @click="removeTodo(todo)" />
                   </div>
                 </div>
               </div>
@@ -51,60 +27,25 @@
             <div flex="~ col">
               <div flex="~ col">
                 <!-- ===========>> Search InPut Content <<=========== -->
-                <UiInput
-                  m="y-20px"
-                  placeholder="ابحث هنا..."
-                  v-model="searchQuery"
-                />
-                <div
-                  v-if="searchQuery != ''"
-                  p="t-15px b-15px"
-                  flex="~ col gap-15px"
-                  w="full"
-                  bg="secondaryOp dark:Secondary opacity-10"
-                  rounded="5px"
-                  overflow="y-scroll"
-                  h="65px"
-                >
+                <UiInput m="y-20px" placeholder="ابحث هنا..." v-model="searchQuery" />
+                <div v-if="searchQuery != ''" p="t-15px b-15px" flex="~ col gap-15px" w="full" bg="secondaryOp dark:Secondary opacity-10" rounded="5px" overflow="y-scroll" h="65px">
                   <div v-for="todo in searchedTodos" :key="todo">
                     <div flex="~" w="full" justify="between" items="center">
-                      <TodoTask
-                        :todo="todo"
-                        v-if="todo.done == true && searchQuery != ''"
-                      />
+                      <TodoTask :todo="todo" v-if="todo.done == true && searchQuery != ''" />
                       <!-- ===========>> Remove Button Content in Search <<=========== -->
-                      <UiButton
-                        w="70px"
-                        title="حذف"
-                        color="error"
-                        @click="removeTodo(todo)"
-                      />
+                      <UiButton w="70px" title="حذف" color="error" @click="removeTodo(todo)" />
                     </div>
                   </div>
                 </div>
               </div>
-              <div
-                m="t-5px"
-                flex="~ col gap-30px"
-                w="full"
-                overflow="y-scroll"
-                h="315px"
-              >
+              <div m="t-5px" flex="~ col gap-30px" w="full" overflow="y-scroll" h="315px">
                 <!-- ===========>> Todo Content <<=========== -->
                 <div v-for="todo in todos_f" :key="todo">
                   <div flex="~" justify="between" items="center" id="todoDiv">
                     <TodoTask :todo="todo" v-if="todo.done == true" />
 
                     <!-- ===========>> Remove Botton Content <<=========== -->
-                    <UiButton
-                      id="markHover"
-                      duration="250"
-                      w="70px"
-                      title="حذف"
-                      color="error"
-                      :loading="false"
-                      @click="removeTodo(todo)"
-                    />
+                    <UiButton id="markHover" duration="250" w="70px" title="حذف" color="error" :loading="false" @click="removeTodo(todo)" />
                   </div>
                 </div>
               </div>
@@ -117,14 +58,15 @@
 </template>
 
 <script setup>
-import { ref, watch, computed, onMounted } from "#imports";
+import { ref, watch, computed, onMounted, useSupabaseClient, useUser, useNuxtApp } from "#imports";
 
 const todos = ref([]);
 const searchQuery = ref("");
 const title = ref("");
 const date = ref("");
-
-const hover = ref();
+const supabase = useSupabaseClient()
+const user = useUser()
+const { $toast } = useNuxtApp()
 
 //All Todos (Active and Done)
 const todos_a = computed(() =>
@@ -140,35 +82,45 @@ const todos_f = computed(() =>
   })
 );
 
-watch(
-  todos,
-  (newVal) => {
-    localStorage.setItem("todos", JSON.stringify(newVal));
-  },
-  {
-    deep: true,
-  }
-);
-
 //Add Todo Function
-const addTodo = () => {
+const addTodo = async () => {
+  if (title.value.length <= 3) {
+    $toast.error('عنوان المهمة لا يمكن ان يكون اقل من 4 حروف');
+    return;
+  }
+  const taskData = {
+    user_id: user.value.id,
+    task: title.value,
+    is_complete: false,
+    inserted_at: new Date(),
+  }
+  let { error, data } = await supabase.from("app_todo").upsert(taskData).select("id").single();
+ 
+  if (error){
+    $toast.error('حدثت مشكلة أثناء الإضافة')
+    return;
+  }
   if (title.value.trim() === "") {
     return;
   }
-  todos.value.push({
+
+  todos.value.unshift({
+    id: data.id,
     title: title.value,
     date: date.value,
     done: false,
-    createdAt: new Date().getTime(),
+    createdAt: new Date(),
   });
-  console.log("Todo Added");
+
   title.value = "";
 };
 
 //Remove Todo Function
-const removeTodo = (todo) => {
-  todos.value = todos.value.filter((t) => t !== todo);
-  console.log("Todo Deleted");
+const removeTodo = async (todo) => {
+  const id = todos.value.find((x) => x.id == todo.id).id;
+  await supabase.from("app_todo").delete().match({ id: id }).then(() => {
+    todos.value = todos.value.filter((t) => t !== todo);
+  });
 };
 
 //Search Todo Function
@@ -180,8 +132,14 @@ const searchedTodos = computed(() => {
   });
 });
 
-onMounted(() => {
-  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+onMounted(async () => {
+  const { data } = await supabase.from('app_todo').select('id, task, is_complete, inserted_at')
+  todos.value = data.map((todo) => todo = {
+    id: todo.id,
+    title: todo.task,
+    date: todo.inserted_at,
+    done: todo.is_complete,  
+  });
 });
 
 const props = defineProps({
