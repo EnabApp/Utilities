cc90955ee13e920c6ed448cb9f65bdab204470c9
Hussein Najah <husseinnajah123@gmail.com>
2022-08-28 00:07:44 +0300

    Beta

--- src/runtime/composables/useMoment.ts
+++ src/runtime/composables/useMoment.ts
@@ -1,6 +1,12 @@
 import moment from "moment/moment.js";
 import ar from "moment/locale/ar";
 
+moment.updateLocale("ar", {
+  longDateFormat: {
+    LT: "h:mm A"
+  },
+});
+
 moment.updateLocale("ar", ar);
 
 export const useMoment = () => {
