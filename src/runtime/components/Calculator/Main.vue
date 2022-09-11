17c84bb8d8d10b9449a5f21a3b2a5a82b7c94600
william-iq397 <93004634+william-iq397@users.noreply.github.com>
2022-09-11 14:10:35 +0300

    the only issue left is useStorage

--- src/runtime/components/Calculator/Main.vue
+++ src/runtime/components/Calculator/Main.vue
@@ -2,55 +2,22 @@
   <!-- Application -->
   <!-- //====== Calculator Screen ======// -->
   <div class="flex flex-col h-full justify-end items-stretch">
-    <div
-      h="2rem"
-      p="x-10px b-5px"
-      text="2xl right primaryOp dark:primary"
-      dir="ltr"
-      w="full"
-      class="text-3xl"
-      ref="windowRef"
-      :class="{
+    <div h="2rem" p="x-10px b-5px" text="2xl right primaryOp dark:primary" dir="ltr" w="full" class="text-3xl"
+      ref="windowRef" :class="{
         'text-4xl': lg,
         'text-5xl': xl || twoXl,
-      }"
-    >
-      <!-- {{ ans.toLocaleString("en-US", { maximumFractionDigits: 8 }) }}
-      {{ operators[operator] }}
-      {{
-        num !== 0
-          ? num.toLocaleString("en-US", {
-              maximumFractionDigits: 8,
-            })
-          : ""
-      }} -->
-      {{ previousValue + operation + currentValue }} {{ resultValue }}
-
-      <CalculatorHistoryIcon
-        v-if="twoXs || xs || sm"
-        @click="toggleModal"
-        class="absolute left-3 bottom-0 top-20 cursor-pointer"
-        w="30px"
-        h="30px"
-      />
+      }">
+      {{ previousValue + operation + currentValue }} <span> {{ resultValue }} </span>
+      <CalculatorHistoryIcon v-if="twoXs || xs || sm" @click="toggleModal"
+        class="absolute left-3 bottom-0 top-20 cursor-pointer" w="30px" h="30px" />
     </div>
     <!-- //====== Buttons styles and loop includes Numbers & Operations ======// -->
     <div m="5px" h="full" flex="~ col" justify="end">
       <div grid="~ cols-10">
-        <div
-          class="grid grid-cols-4 gap-1px col-span-10"
-          :class="{ 'col-span-7': twoXl || xl || lg || md, 'col-span-10': sm || xs || twoXs}"
-        >
-          <button
-            overflow="auto"
-            cursor="pointer"
-            duration="150"
-            rounded="5px"
-            p="y-10px"
-            w="full"
-            font="semibold"
-            border="none"
-            :class="{
+        <div class="grid grid-cols-4 gap-1px col-span-10"
+          :class="{ 'col-span-7': twoXl || xl || lg || md, 'col-span-10': sm || xs || twoXs}">
+          <button overflow="auto" cursor="pointer" duration="150" rounded="5px" p="y-10px" w="full" font="semibold"
+            border="none" :class="{
               'text-lg': twoXs,
               'text-xl': xs,
               'text-2xl': sm,
@@ -67,29 +34,24 @@
                 ((index + 1) % 4 !== 0 || index < 7 || index !== 20) &&
                 button !== '=' &&
                 button !== 'C',
-            }"
-            v-for="(button, index) in buttons"
-            :key="index"
-            @click="calculate(button)"
-            type="button"
-          >
+            }" v-for="(button, index) in buttons" :key="index" @click="calculate(button)" type="button">
             {{ button }}
           </button>
         </div>
         <!-- //====== Calculator History FOR LARGE SCREEN SIZES ======// -->
-        <CalculatorHistory
-          :historyList="historyList"
-          :BreakpointWindow="BreakpointWindow"
-        />
+        <CalculatorHistory :historyList="historyList" :BreakpointWindow="BreakpointWindow" />
         <!-- DISPLAY THE HISTORY FOR SMALL SCREEN SIZES -->
         <Teleport to="body">
           <UiModal v-model="stateModal" @cancel="modalCanceled">
             <template v-slot:title>Calculation History</template>
-              <div class="h-32" overflow="y-scroll" flex="~ col gap-1" relative>
-                <p v-if="historyList[0]" class="absolute left-2 top-0 py-3 px-5 bg-white text-center text-black cursor-pointer rounded-lg" @click="clearHistory">clear</p>
-                <p class="text-center" v-else>لم يتم حساب اي عمليات</p>
-                <h5 class="flex justify-end" dir="ltr" v-for="(result, index) in historyList" :key="index">{{ result}}</h5>
-              </div>
+            <div class="h-32" overflow="y-scroll" flex="~ col gap-1" relative>
+              <p v-if="historyList[0]"
+                class="absolute left-2 top-0 py-3 px-5 bg-white text-center text-black cursor-pointer rounded-lg"
+                @click="clearHistory">clear</p>
+              <p class="text-center" v-else>لم يتم حساب اي عمليات</p>
+              <h5 class="flex justify-end" dir="ltr" v-for="(result, index) in historyList" :key="index">{{ result}}
+              </h5>
+            </div>
           </UiModal>
         </Teleport>
       </div>
@@ -116,7 +78,8 @@ const props = defineProps({
 
 // FUNCTION TO CLEAR THE HISTORY IN UIMODAL
 function clearHistory() {
-  while(historyList.value.length > 0) {
+  while (historyList.value.length > 0)
+  {
     historyList.value.pop()
   }
 }
@@ -172,7 +135,6 @@ let currentValue = ref("");
 let previousValue = ref("");
 let operation = ref("");
 let resultValue = ref("");
-let numberAfterEqual = ref("");
 
 // PROCESS OF CALCULATION AND RESULT VARIABLES
 let processOfCalculation = ref("");
@@ -184,13 +146,18 @@ let historyStorage = ref("")
 
 function calculate(button) {
   // adding numbers to the screen
-  if (!isNaN(button) || button == ".") {
-    if (currentValue.value.length <= 10) {
+  if (!isNaN(button) || button == ".")
+  {
+    if (currentValue.value.length <= 10)
+    {
       currentValue.value += button;
+      // set result value to ""
     }
   }
+
   // clear the numbers
-  if (button === "C") {
+  if (button === "C")
+  {
     operation.value = "";
     previousValue.value = "";
     currentValue.value = "";
@@ -198,20 +165,24 @@ function calculate(button) {
     resultValue.value = ""
   }
   // split between numbers before the operator and after the operator
-  if (["×", "+", "-", "÷"].includes(button)) {
-    previousValue.value = currentValue.value;
+  if (["×", "+", "-", "÷"].includes(button))
+  {
+    previousValue.value = resultValue.value ? resultValue.value : currentValue.value;
     currentValue.value += button;
     operation.value = button;
     currentValue.value = "";
+    resultValue.value = ""
   }
   // GETTING THE REST OF DIVIDING
-  if (button === "%") {
+  if (button === "%")
+  {
     currentValue.value = currentValue.value / 100;
   }
   console.log(currentValue.value.length)
 
   // DELETE THE LAST NUMBER
-  if (button === "Del") {
+  if (button === "Del")
+  {
     currentValue.value = currentValue.value.substring(
       0,
       currentValue.value.length - 1
@@ -219,40 +190,48 @@ function calculate(button) {
   }
 
   // multiply the number by himself
-  if (button === "x²") {
+  if (button === "x²")
+  {
     currentValue.value = currentValue.value * currentValue.value
   }
 
   // the square root of the number
-  if (button === '√x') {
+  if (button === '√x')
+  {
     currentValue.value = Math.sqrt(currentValue.value)
   }
 
   // get the 1/X of the number
-  if (button === "1/x") {
+  if (button === "1/x")
+  {
     currentValue.value = eval(1 / currentValue.value)
   }
 
   // if it is "π" then set the value to 3.14
-  if (button === "π") {
+  if (button === "π")
+  {
     currentValue.value = "3.14"
   }
 
   // if the number is positive then set it to negative else set it to positive
-  if (button === "+/-" && currentValue.value != 0 ) {
+  if (button === "+/-" && currentValue.value != 0)
+  {
     currentValue.value = currentValue.value > 0 ? currentValue.value * -1 : currentValue.value * -1
   }
 
   // GET THE RESULT OF THE OPERATION
-  if (button === "=") {
+  if (button === "=")
+  {
 
     // turning multiply into '*' to be easy calculated
-    if (operation.value === "×") {
+    if (operation.value === "×")
+    {
       operation.value = "*";
     }
 
     // turning divide into '/' to be easy calculated
-    if (operation.value === "÷") {
+    if (operation.value === "÷")
+    {
       operation.value = '/'
     }
 
@@ -266,19 +245,17 @@ function calculate(button) {
       previousValue.value + operation.value + currentValue.value
     );
 
-
     // prepare the data to push to the history
     resultInHistory.value =
-      processOfCalculation.value + " " + "=" + " " + resultValue.value;
+      processOfCalculation.value + " " + "=" + " " +  (resultValue.value ? resultValue.value : currentValue.value);
 
     // push the data to the history if there is an operation
-    if (operation.value) {
+    if (operation.value)
+    {
       historyList.value.push(resultInHistory.value);
     }
 
     /* PUSH useStorage HERE */
-
-
 
     // remove the numbers from the result bar WHEN CLICK THE EQUAL BUTTON
     previousValue.value = "";
@@ -291,23 +268,23 @@ function calculate(button) {
 </script>
 
 <style scoped>
-  /* width */
-  ::-webkit-scrollbar {
-    width: 10px;
-  }
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
 
-  /* Track */
-  ::-webkit-scrollbar-track {
-    background: #f1f1f1;
-  }
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
 
-  /* Handle */
-  ::-webkit-scrollbar-thumb {
-    background: #888;
-  }
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
 
-  /* Handle on hover */
-  ::-webkit-scrollbar-thumb:hover {
-    background: #555;
-  }
-  </style>
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
